fa9857951dc882a132acca08e0e63e479fce6e35
ZorianaCherevko <89517545+ZorianaCherevko@users.noreply.github.com>
2021-08-25 12:26:40 +0200

    Add files via upload

---
+++ Tast 1.2.Conditions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace switch678
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, sum = 0,x;
+            n = int.Parse(Console.ReadLine());
+
+            while (n > 0)
+            {
+                x = n % 10;
+                n = n / 10;
+                if (x % 2 != 1)
+                    sum = sum + x;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
+
