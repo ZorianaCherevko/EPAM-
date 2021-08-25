65c228faf8f93033354fff56cecfb9333ddfbd20
ZorianaCherevko <89517545+ZorianaCherevko@users.noreply.github.com>
2021-08-25 12:15:55 +0200

    Add files via upload

---
+++ Task1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Blabla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            if (n == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                if (n < 0)
+                {
+                    int x = n * -1;
+                    Console.WriteLine(x);
+                }
+                else
+                {
+                    Console.WriteLine(n*n);
+                }
+            }
+
+        }
+    }
+}
+
+
